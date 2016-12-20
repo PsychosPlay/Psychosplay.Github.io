b3ae2d9bd37472c771be934e3fe976684a55a6aa
Jeff Downie <jeff.downie@blueyonder.co.uk>
2016-12-20 15:58:35 +0000

    Switching to tabs, not spaces.
    Apologies! I didn't notice that tabs were being used here!

--- engine.js
+++ engine.js
@@ -5710,42 +5710,42 @@ var Engine = {
 };
 
 function UnlockX(X) {
-    return function() {
-        if (Engine.Player.Money > Engine.Player.GenCost) {
-            if (Engine.Player["Gen" + X] < 1) {
-                Engine.Player["Gen" + X]++;
-                Engine.Player.Generations++;
-                Engine.Player.Money -= Engine.Player.GenCost;
-                Engine.Player.GenCost = 1000*Math.pow(5, Engine.Player.Generations);
-                Engine.Status(X + " License Purchased!");
-            } else {
-                Engine.Status(X + " License already bought!");
-            }
-        } else {
-            Engine.Status("Not enough money for a new license!");
-        }
-    };
+	return function() {
+		if (Engine.Player.Money > Engine.Player.GenCost) {
+			if (Engine.Player["Gen" + X] < 1) {
+				Engine.Player["Gen" + X]++;
+				Engine.Player.Generations++;
+				Engine.Player.Money -= Engine.Player.GenCost;
+				Engine.Player.GenCost = 1000*Math.pow(5, Engine.Player.Generations);
+				Engine.Status(X + " License Purchased!");
+			} else {
+				Engine.Status(X + " License already bought!");
+			}
+		} else {
+			Engine.Status("Not enough money for a new license!");
+		}
+	};
 };
 
 function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+	// Declare all variables
+	var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+	// Show the current tab, and add an "active" class to the link that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
 }
 
 window.onload = function() {
